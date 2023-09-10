7510cd4b370780aedff4f9c2a49e5710b2e48f04
isha-ingersol <isha.ingersol@gmail.com>
2023-09-10 18:29:31 +0400

    Modified About Section

--- src/components/About.js
+++ src/components/About.js
@@ -9,6 +9,7 @@ import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png"
 import colorSharp2 from "../assets/img/color-sharp2.png"
 import EWCIsha from "../assets/img/ewc-semifinal1-02.jpg"
+import { ArrowRightCircle } from 'react-bootstrap-icons';
 
 export const About = () => {
   return (
@@ -22,29 +23,25 @@ export const About = () => {
                         <h2>About Me</h2>
                         <img className="EWCIsha1" src={EWCIsha} alt="Image of Isha Ingersol" />
                         <p>
-                            I am <span>Isha Ingersol</span>, a 19-year-old from India. 
-                            Coding is my passion, and I aspire to become a Software Developer or a Full-Stack Web Developer.
+                            I am <span>Isha Ingersol</span>, a 19-year-old from India.
                             <br></br>
                             <br></br>
-                            During my undergraduate program at the University of Birmingham Dubai, I had the honor of being selected as 
-                            the <span>Student Representative</span> for Computer Science students in my sophomore year (Year 2). 
-                            Additionally, I have been serving as a <span>Student Ambassador</span> for Open Days and Welcome Weeks, 
-                            where I assist prospective students and guide new students to the university, ensuring they feel welcomed. 
-                            Recently, I also took on the role of a <span>Marketing Student Ambassador</span> for the university.
+                            As an undergraduate, I took up leadership roles at the university as the <span>Computer Science Student Representative </span> 
+                            (in my sophomore year/Year 2), <span>Marketing Student Ambassador</span>, and as a <span>Student Ambassador</span> for Open Days and Welcome Weeks.
                             <br></br>
                             <br></br>
-                            Throughout my university journey, I have completed various projects, including <span>web development</span>, 
-                            developing a <span>software solution</span> (theoretical), a colossal adventure game, and a text-based BINGO game. 
-                            These projects have allowed me to gain <span>hands-on experience</span> and enhance my skills. 
+                            I am proficient in a variety of programming languages and web development technologies, and have <span>hands-on experience </span> 
+                            in developing various projects, in <span>full stack web development</span> and <span>game development</span>.
                             <br></br>
                             <br></br>
-                            I am currently enrolled in <span>Harvard University's CS50x</span> course, which has been incredibly interesting. 
+                            {/* I am currently enrolled in <span>Harvard University's CS50x</span> course, which has been incredibly interesting. 
                             The course covers the basics of Computer Science, along with Python, HTML, CSS, JavaScript, Scratch, Data Structures and Algorithms, Flask, and more. 
                             I plan to continue my learning journey by pursuing the <span>CS50P</span> and <span>CS50AI</span> courses (offered by Harvard University) in the future.
                             <br></br>
-                            <br></br>
+                            <br></br> */}
                             Apart from academics and tech, I have a keen interest in playing <span>badminton</span>, exploring 
-                            <span>different varieties of food</span>, watching <span>"Shark Tank"</span>, and <span>driving</span>.
+                            <span> different varieties of food</span>, watching <span>"Shark Tank"</span>, and <span>driving</span>.
+                            <a href="#connect">Got any questions? Reach out to me now! <ArrowRightCircle size={25} /></a>
                         </p>
                     </div>
                 </div>
